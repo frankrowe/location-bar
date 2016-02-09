b83199b2f0e30935bbd9da22b312ad6b76314fd6
Frank Rowe <fsrowe@gmail.com>
2016-02-09 11:45:20 -0500

    metadata

--- location-bar.1m.sh
+++ location-bar.1m.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# <bitbar.title>location-bar</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Frank Rowe</bitbar.author>
+# <bitbar.author.github>frankrowe</bitbar.author.github>
+# <bitbar.desc>Displays the current GPS coordinates (latitude,longitude)</bitbar.desc>
+# <bitbar.image>http://frankrowe.org/img/location-bar.png</bitbar.image>
+# <bitbar.dependencies>whereami</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/frankrowe/location-bar</bitbar.abouturl>
+
 WHEREAMI="$HOME/whereami"
 
 LAT=`$WHEREAMI | grep Latitude  | awk -F" " '{print $2}' | awk '{print $1}'`
